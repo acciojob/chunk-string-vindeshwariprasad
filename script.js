fd6c66d47da4051cc2d1924042f4290df43ab786
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 05:01:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function stringChop(str, size) {
+	if (!str || str.length === 0) {
+        return [];
+    }
+	size = parseInt(size);
+    
+    // Initialize an empty array to store the chunks
+    const chunks = [];
+    
+    // Iterate over the input string and slice it into chunks
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.slice(i, i + size));
+    }
+    
+    // Return the array of chunks
+    return chunks;
   // your code here
 }
 
